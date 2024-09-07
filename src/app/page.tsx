f1dbafa1cd8510bf4517b3faff4b86167fea95ad
Sarah Pickrahn <spickrahn@gmail.com>
2024-09-07 15:04:40 -0700

    small home page cleanup

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,9 @@
-import Image from "next/image";
-
-export default function Home() {
+const Home = async () => {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <h1>OnlyCats</h1>
     </main>
   );
-}
+};
+
+export default Home;
