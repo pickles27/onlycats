a8a1dc19ba5f8c40e8728f6ffd914be6613b8d46
Sarah Pickrahn <spickrahn@gmail.com>
2024-09-07 19:59:32 -0700

    add temporary config

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,13 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "preview.redd.it",
+      },
+    ],
+  },
+};
 
 export default nextConfig;
