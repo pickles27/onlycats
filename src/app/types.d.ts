d2c9dd668d7464bba7de755e8512ef5e4a6ce5ad
Sarah Pickrahn <spickrahn@gmail.com>
2024-09-07 19:58:38 -0700

    remove unused types

--- src/app/types.d.ts
+++ src/app/types.d.ts
@@ -1,16 +1,1 @@
 export type ActionType = "LIKE" | "UPLOAD";
-
-export interface Action {
-  action: ActionType;
-  actionId: string;
-  createdAt: string;
-  ipAddress: string;
-  postId: string;
-}
-
-export interface Post {
-  createdAt: string;
-  imageUrl: string;
-  likes: number;
-  postId: string;
-}
