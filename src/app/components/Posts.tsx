a315cca4becdca0502d59e385f0ba0e1575f1f73
Sarah Pickrahn <spickrahn@gmail.com>
2024-09-07 19:58:09 -0700

    create feed component

---
+++ src/app/components/Posts.tsx
@@ -0,0 +1,15 @@
+import { Post } from "./Post";
+
+export interface PostsProps {
+  posts: { postId: number; imageUrl: string }[];
+}
+
+export const Posts = ({ posts }: PostsProps) => {
+  return (
+    <div className="w-full grid grid-cols-1 lg:grid-cols-2 gap-4 space-between">
+      {posts.map((post) => (
+        <Post key={post.postId} imageUrl={post.imageUrl} />
+      ))}
+    </div>
+  );
+};
