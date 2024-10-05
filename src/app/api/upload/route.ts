6607b461c0cfd8966c18ad0040b894a834a9efb9
Sarah Pickrahn <spickrahn@gmail.com>
2024-10-05 16:12:00 -0700

    allow users to upload photos

---
+++ src/app/api/upload/route.ts
@@ -0,0 +1,127 @@
+import { NextResponse } from "next/server";
+import { Storage } from "@google-cloud/storage";
+
+const storage = new Storage({
+  projectId: process.env.GOOGLE_CLOUD_PROJECT_ID,
+  credentials: {
+    client_email: process.env.GOOGLE_CLOUD_CLIENT_EMAIL,
+    private_key: process.env.GOOGLE_CLOUD_PRIVATE_KEY?.split(
+      String.raw`\n`
+    ).join("\n"),
+  },
+});
+
+const bucketName = process.env.GOOGLE_CLOUD_BUCKET_NAME;
+const bucket = storage.bucket(bucketName!);
+
+interface ParsedFile {
+  filename: string;
+  mimeType: string;
+  buffer: Buffer;
+}
+
+interface ParsedData {
+  [key: string]: ParsedFile | string;
+}
+
+const parseMultipart = (buffer: Buffer, boundary: string): ParsedData => {
+  const boundaryBuffer = Buffer.from(`--${boundary}`);
+  const result: ParsedData = {};
+  let currentIndex = buffer.indexOf(boundaryBuffer);
+
+  while (currentIndex !== -1) {
+    const startIndex = currentIndex + boundaryBuffer.length;
+    const endIndex = buffer.indexOf(boundaryBuffer, startIndex);
+    if (endIndex === -1) {
+      break;
+    }
+
+    const part = buffer.subarray(startIndex, endIndex).toString();
+    const headerEndIndex = part.indexOf("\r\n\r\n");
+    if (headerEndIndex === -1) {
+      currentIndex = endIndex;
+      continue;
+    }
+
+    const header = part.slice(0, headerEndIndex);
+    const body = buffer.subarray(
+      currentIndex + boundaryBuffer.length + headerEndIndex + 4,
+      startIndex
+    );
+
+    const nameMatch = header.match(/name="([^"]+)"/);
+    const filenameMatch = header.match(/filename="([^"]+)"/);
+    const contentTypeMatch = header.match(/Content-Type: (.+)/);
+
+    if (nameMatch) {
+      const name = nameMatch[1];
+      if (filenameMatch) {
+        const filename = filenameMatch[1];
+        const mimeType = contentTypeMatch
+          ? contentTypeMatch[1]
+          : "application/octet-stream";
+
+        result[name] = { filename, mimeType, buffer: body };
+      } else {
+        result[name] = body.toString();
+      }
+    }
+
+    currentIndex = startIndex;
+  }
+
+  return result;
+};
+
+export async function POST(req: Request) {
+  try {
+    const contentType = req.headers.get("content-type");
+    if (!contentType) {
+      return NextResponse.json(
+        { message: "Content type not found." },
+        { status: 400 }
+      );
+    }
+
+    const boundary = contentType.split("=")[1];
+    const buffer = Buffer.from(await req.arrayBuffer());
+    const parsedData = parseMultipart(buffer, boundary);
+
+    if (!parsedData.image || !(parsedData.image as ParsedFile).buffer) {
+      return NextResponse.json(
+        { message: "No image file found in the request." },
+        { status: 400 }
+      );
+    }
+
+    const imageFile = parsedData.image as ParsedFile;
+
+    const blob = bucket.file(`cats/${imageFile.filename}`);
+    const blobStream = blob.createWriteStream({
+      resumable: false,
+      metadata: {
+        contentType: imageFile.mimeType,
+      },
+    });
+
+    await new Promise<void>((resolve, reject) => {
+      blobStream.on("error", (error) => {
+        reject(error);
+      });
+
+      blobStream.on("finish", () => {
+        resolve();
+      });
+
+      blobStream.end(imageFile.buffer);
+    });
+
+    const publicUrl = `https://storage.googleapis.com/${bucket.name}/${blob.name}`;
+    return NextResponse.json({ fileUrl: publicUrl });
+  } catch (error) {
+    return NextResponse.json(
+      { message: "Error handling upload: " + (error as Error).message },
+      { status: 500 }
+    );
+  }
+}
--- src/app/components/UploadButton.tsx
+++ src/app/components/UploadButton.tsx
@@ -2,19 +2,90 @@
 
 "use client";
 
+import { ChangeEvent, useState } from "react";
+import toast from "react-hot-toast";
+
 export const UploadButton = () => {
-  const handleUploadClick = () => {
-    // todo: add upload functionality
+  const [isUploading, setIsUploading] = useState(false);
+
+  const handleFileChange = async (e: ChangeEvent<HTMLInputElement>) => {
+    const file = e.target.files?.[0];
+
+    if (!file) {
+      toast.error("Please select a file to upload.");
+      return;
+    }
+
+    setIsUploading(true);
+    const formData = new FormData();
+    formData.append("image", file);
+
+    try {
+      const response = await fetch("/api/upload", {
+        method: "POST",
+        body: formData,
+      });
+
+      const result = await response.json();
+
+      if (!response.ok) {
+        throw new Error(`Response not ok. Message: ${result.message}`);
+      }
+
+      toast.success("Cat uploaded successfully!");
+    } catch (error) {
+      toast.error("Cat failed to upload.");
+    } finally {
+      e.target.value = "";
+      setIsUploading(false);
+    }
   };
 
   return (
-    <button
-      aria-label="Upload"
-      className="flex items-center gap-1 bg-sky-500/10 hover:bg-sky-500/40 rounded-md py-0.5 px-3"
-      onClick={handleUploadClick}
-    >
-      <img alt="Upload icon" className="w-8" src="../upload.png" />
-      Upload
-    </button>
+    <form>
+      <label
+        htmlFor="file-upload"
+        className="w-28 h-10 flex justify-center items-center gap-1 bg-sky-500/10 hover:bg-sky-500/40 rounded-md py-1 px-3 cursor-pointer"
+      >
+        {isUploading ? (
+          <LoadingSpinner />
+        ) : (
+          <div className="flex items-center gap-1">
+            Upload
+            <img alt="Upload icon" className="w-8" src="../upload.png" />
+          </div>
+        )}
+      </label>
+      <input
+        id="file-upload"
+        accept="image/*"
+        onChange={handleFileChange}
+        type="file"
+        className="hidden"
+      />
+    </form>
   );
 };
+
+const LoadingSpinner = () => (
+  <svg
+    className="animate-spin h-5 w-5 text-blue-500"
+    xmlns="http://www.w3.org/2000/svg"
+    fill="none"
+    viewBox="0 0 24 24"
+  >
+    <circle
+      className="opacity-25"
+      cx="12"
+      cy="12"
+      r="10"
+      stroke="currentColor"
+      strokeWidth="4"
+    ></circle>
+    <path
+      className="opacity-75"
+      fill="currentColor"
+      d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4z"
+    ></path>
+  </svg>
+);
