fad5c7051f6af43355c24356bcf67490745c0bc5
Sarah Pickrahn <spickrahn@gmail.com>
2024-11-03 18:32:50 -0800

    adjust padding

--- src/app/components/Posts.tsx
+++ src/app/components/Posts.tsx
@@ -16,7 +16,7 @@ export const Posts = async () => {
   const posts = await response.json();
 
   return (
-    <div className="w-full grid grid-cols-1 md:grid-cols-3 gap-4 space-between">
+    <div className="w-full grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4 space-between">
       {posts.map((post: any) => (
         <Image
           key={post.post_id}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,15 +4,15 @@ import { UploadButton } from "./components/UploadButton";
 
 const Home = async () => {
   return (
-    <main className="flex min-h-screen flex-col items-center px-8 md:px-24 pt-8">
+    <main className="flex min-h-screen flex-col items-center px-8 sm:px-12 lg:px-16 pt-8">
       <div className="w-full flex justify-between items-center gap-2 mb-6">
         <div className="flex">
           <img
             alt="cat logo"
-            className="w-8 md:w-10 h-8 md:h-10 object-contain"
+            className="w-8 lg:w-10 h-8 lg:h-10 object-contain"
             src="../cat.png"
           />
-          <h1 className="text-sky-500 font-bold text-2xl md:text-3xl">
+          <h1 className="text-sky-500 font-bold text-2xl lg:text-3xl">
             OnlyCats
           </h1>
         </div>
