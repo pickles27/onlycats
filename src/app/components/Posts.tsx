9cb3c075c591bc5f407167099fc0633f302c8e89
Sarah Pickrahn <spickrahn@gmail.com>
2024-11-03 18:27:15 -0800

    use 3 columns

--- src/app/components/Posts.tsx
+++ src/app/components/Posts.tsx
@@ -16,7 +16,7 @@ export const Posts = async () => {
   const posts = await response.json();
 
   return (
-    <div className="w-full grid grid-cols-1 md:grid-cols-2 gap-4 space-between">
+    <div className="w-full grid grid-cols-1 md:grid-cols-3 gap-4 space-between">
       {posts.map((post: any) => (
         <Image
           key={post.post_id}
