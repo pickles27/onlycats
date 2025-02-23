3714eda736d077ccd1d55e591ddd0bbbd9ab3560
Sarah Pickrahn <spickrahn@gmail.com>
2025-02-23 15:57:21 -0800

    remove import

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,4 @@
 import type { Metadata } from "next";
-import { SpeedInsights } from "@vercel/speed-insights/next";
 import { Analytics } from "@vercel/analytics/react";
 import { Inter } from "next/font/google";
 import { Toaster } from "react-hot-toast";
