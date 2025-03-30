8b6b3aa3331450af413b66f05bf933eea519f077
Sarah Pickrahn <spickrahn@gmail.com>
2025-03-30 12:28:19 -0700

    fix header padding on mobile

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -3,7 +3,7 @@ import catIcon from "public/cat.svg";
 import Image from "next/image";
 
 export const Header = () => (
-  <div className="fixed top-0 z-50 bg-white w-full flex justify-between items-center gap-2 py-2 px-4s sm:px-12 lg:px-16">
+  <div className="fixed top-0 z-50 bg-white w-full flex justify-between items-center gap-2 py-2 px-4 sm:px-12 lg:px-16 border-b-sky-600">
     <div className="flex items-center gap-1">
       <Image alt="cat icon" src={catIcon} />
       <h1 className="text-sky-600 text-3xl font-bold drop-shadow-sm">
