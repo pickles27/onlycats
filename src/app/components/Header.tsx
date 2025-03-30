dfd0606d90013b8dee9b5f131243262b7e154bd2
Sarah Pickrahn <spickrahn@gmail.com>
2025-03-30 12:40:57 -0700

    clicking header icon/text links to home

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -1,15 +1,16 @@
 import { UploadButton } from "./UploadButton";
 import catIcon from "public/cat.svg";
 import Image from "next/image";
+import Link from "next/link";
 
 export const Header = () => (
   <div className="fixed top-0 z-50 bg-white w-full flex justify-between items-center gap-2 py-2 px-4 sm:px-12 lg:px-16 border-b-sky-600">
-    <div className="flex items-center gap-1">
+    <Link className="flex items-center gap-1" href="/">
       <Image alt="cat icon" src={catIcon} />
       <h1 className="text-sky-600 text-3xl font-bold drop-shadow-sm">
         OnlyCats
       </h1>
-    </div>
+    </Link>
     <UploadButton />
   </div>
 );
